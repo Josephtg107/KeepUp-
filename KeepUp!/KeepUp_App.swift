d6c8511cd0385fd57f797aef445a17e0498386ab
Jose Teodoro Garcia <99679017+Josephtg107@users.noreply.github.com>
2023-08-21 19:04:25 -0500

    Initial Commit

---
+++ KeepUp!/KeepUp_App.swift
@@ -0,0 +1,17 @@
+//
+//  KeepUp_App.swift
+//  KeepUp!
+//
+//  Created by Joseph Garcia on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct KeepUp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeepUp!UITests/KeepUp_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KeepUp_UITestsLaunchTests.swift
+//  KeepUp!UITests
+//
+//  Created by Joseph Garcia on 21/08/23.
+//
+
+import XCTest
+
+final class KeepUp_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
